3b09c45cfe735494d3b22d44ab8645021b64e8e0
Evgenstreet <84145620+Evgenstreet@users.noreply.github.com>
2022-11-20 23:36:36 +0300

    Update lab9 repo
    Add before&after methods

--- WebDriverWithSelenium/src/test/java/WebDriverLab9Test.java
+++ WebDriverWithSelenium/src/test/java/WebDriverLab9Test.java
@@ -2,9 +2,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -13,9 +16,19 @@ import java.time.Duration;
 public class WebDriverLab9Test {
     private WebDriver driver;
 
+    @BeforeMethod(alwaysRun = true)
+    public void browserSetup(){
+        ChromeOptions settings = new ChromeOptions();
+        settings.addArguments("--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080",
+                "--disable-extensions", "--proxy-server='direct://'", "--proxy-bypass-list=*", "--start-maximized",
+                "--disable-gpu", "--ignore-certificate-errors");
+        driver = new ChromeDriver(settings);
+        driver.manage().window().maximize();
+    }
+
+
     @Test
     public void addWatchesFortressGMTGreenToCart() throws InterruptedException {
-        driver = new ChromeDriver();
         driver.get("https://graham1695.com/");
 
         WebElement buttonShopWatches = (new WebDriverWait(driver, Duration.ofSeconds(5)))
@@ -29,7 +42,15 @@ public class WebDriverLab9Test {
         WebElement buttonAddToCart;
         buttonAddToCart = driver.findElement(By.id("AddToCartText-6591917621312"));
         buttonAddToCart.click();
+        Thread.sleep(5000);
 
         Assert.assertNotNull(driver.findElement(By.id("CartContainer")));
+    }
+
+
+    @AfterTest(alwaysRun = true)
+    public void browserTakeDown() {
+        driver.quit();
+        driver = null;
     }
 }
